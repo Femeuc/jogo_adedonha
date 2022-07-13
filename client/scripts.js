2cd93021aa269e56c189e626d9a1e4dc7b13ee18
= <femeuf5@gmail.com>
2022-07-13 01:41:25 -0300

    Progress on game state 0

--- client/scripts.js
+++ client/scripts.js
@@ -1,3 +1,19 @@
+function server_state() {
+    socket.emit("SERVER_STATE", (state) => {
+        console.log(state);
+    });
+}
+
+document.querySelector("#send_message").addEventListener("keydown", function(event) {
+    let ul_chat = document.querySelector('ul#chat');
+    if (event.key === "Enter") {
+        ul_chat.innerHTML += `<li>${event.target.value}</li>`;
+        console.log(`MESSAGE SENT: ${event.target.value}`);
+        event.target.value = '';
+    }
+});
+
+/* #region Connection related functions */
 function create_room() {
     const inputs = document.querySelectorAll('#enter_room input');
     const username = inputs[0].value.toLowerCase();
@@ -40,12 +56,6 @@ function enter_room() {
 
         console.log(`User "${username}" enters room named "${room}"`);
         load_room( room_obj );
-    });
-}
-
-function server_state() {
-    socket.emit("SERVER_STATE", (state) => {
-        console.log(state);
     });
 }
 
@@ -98,8 +108,33 @@ function update_chat_bar(users) {
 
 function load_game_state_0(users) {
 }
+/* #endregion */
 
 function get_random_avatar_name() {
     const random = Math.floor(Math.random() * 12);
     return `among_${random}.PNG`;
 }
+
+function toggle_checkbox( span ) {
+    const input_span = span.querySelectorAll('span')[0];
+    const letter = span.querySelectorAll('span')[1];
+    const input = input_span.querySelector('input');
+
+    input_span.classList.toggle("checked");
+    input.checked = input_span.classList.contains("checked");
+    console.log(`CHECKBOX_CHANGE: ${letter.innerText} is ${input.checked}`);
+}
+
+function add_custom_topic() {
+    const div = document.querySelector('#custom_topics_div');
+    const input = document.querySelector('#custom_topic');
+    div.innerHTML += `
+        <span class="checkbox_container" onclick="toggle_checkbox(this)">
+            <span class="checkbox checked"> <input type="checkbox" checked> </span>
+            <span>${input.value}</span>
+        </span>
+    `;
+    console.log(`Novo tópic: ${input.value}`);
+    input.value = '';
+}
+
